b563cdf25ddebbb3ba7544845359ca0c1b8024d5
gleehave <gleehave@gmail.com>
2026-02-22 17:56:28 +0900

    [add] function

--- video_status.py
+++ video_status.py
@@ -6,5 +6,18 @@ CHANNEL_HANDLE = "MrBeast"
 
 URL = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
-response = requests.get(URL)
-print(json.dumps(response.json(), indent=4))
+
+def get_playlist_id():
+    try:
+    
+        response = requests.get(URL)
+        response.raise_for_status()
+        data = response.json()
+        channel_items = data['items'][0]
+        return channel_items['contentDetails']['relatedPlaylists']['uploads']
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
+if __name__ == "__main__":
+    print(get_playlist_id())
