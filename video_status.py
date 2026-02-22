e58062372d3216daa4edf28bd0b0783ac0d3ae23
gleehave <gleehave@gmail.com>
2026-02-22 18:09:27 +0900

    [add] venv & env

--- video_status.py
+++ video_status.py
@@ -1,7 +1,11 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
 
-API_KEY = ""
+load_dotenv(dotenv_path="./.env")
+
+API_KEY = os.getenv('API_KEY')
 CHANNEL_HANDLE = "MrBeast"
 
 URL = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
