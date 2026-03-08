26e1bc301460916a5104c5699579bb03d9548398
gleehave <gleehave@gmail.com>
2026-03-08 13:06:10 +0900

    [add] extract video meta data

--- dags/api/video_status.py
+++ dags/api/video_status.py
@@ -51,6 +51,44 @@ def get_video_ids(playlist_id):
     except requests.exceptions.RequestException as e:
         raise e
 
+
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_id_list, batch_size):
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id: video_id + batch_size]
+    
+    try:
+        for batch in batch_list(video_ids, maxResults):
+            video_ids_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id={video_ids_str}&key={API_KEY}"
+            
+            response = requests.get(url)
+            response.raise_for_status()
+            
+            for item in data.get("items",[]):
+                video_id = item["id"]
+                snippet = item["snippet"]
+                contentDetails = item["contentDetails"]
+                statistics = item["statistics"]
+            
+                extract_video_data.append({
+                    "video_id": video_id,
+                    "title": snippet["title"],
+                    "publishedAt": snippet["publishedAt"],
+                    "duration": contentDetails["duration"],
+                    "viewCount": statistics["viewCount"],
+                    "likeCount": statistics["likeCount"],
+                    "commentCount": statistics["commentCount"]
+                })
+        return extracted_data
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
-    get_video_ids(playlist_id)
+    video_ids = get_video_ids(playlist_id)
+    extract_video_data(video_ids)
